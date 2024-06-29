5885ce4954001ed1ae44533eab215c661e08469f
preeteshshan09 <preeteshshan09@gmail.com>
2024-06-29 17:43:37 +0530

    initial html

---
+++ App.js
@@ -0,0 +1,14 @@
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "childA" }, [
+    React.createElement("h1", { id: "child11" }, "i am child no 1"),
+    React.createElement("h2", { id: "child21" }, "i am child no 2"),
+  ]),
+  React.createElement("div", { id: "childB" }, [
+    React.createElement("h1", { id: "child12" }, "i am child no 1"),
+    React.createElement("h2", { id: "child22" }, "i am child no 2"),
+  ]),
+]);
+
+// const header = React.createElement("h1", {}, "hello world!!! ");
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
